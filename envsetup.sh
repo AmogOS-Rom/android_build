LMODroid/platform_build

Add customer customization framework
Change-Id: Ic864a578d3968475d252173968ca938ca42cf812

envsetup.sh
@@ -59,7 +59,7 @@ EOF
     local T=$(gettop)
     local A=""
     local i
-    for i in `cat $T/build/envsetup.sh $T/vendor/lmodroid/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
+    for i in `cat $T/build/envsetup.sh $T/vendor/lmodroid/build/envsetup.sh $T/$CUSTOMER_VENDOR_DIR/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
       A="$A $i"
     done
     echo $A
@@ -70,8 +70,8 @@ function build_build_var_cache()
 {
     local T=$(gettop)
     # Grep out the variable names from the script.
-    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/lmodroid/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
-    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/lmodroid/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/lmodroid/build/envsetup.sh $T/$CUSTOMER_VENDOR_DIR/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/lmodroid/build/envsetup.sh $T/$CUSTOMER_VENDOR_DIR/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
     # Call the build system to dump the "<val>=<value>" pairs as a shell script.
     build_dicts_script=`\builtin cd $T; build/soong/soong_ui.bash --dumpvars-mode \
                         --vars="${cached_vars[*]}" \
@@ -153,11 +153,7 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-    if (echo -n $1 | grep -q -e "^lmodroid_") ; then
-        LMODROID_BUILD=$(echo -n $1 | sed -e 's/^lmodroid_//g')
-    else
-        LMODROID_BUILD=
-    fi
+    LMODROID_BUILD=$(__customer_check_product $1)
     export LMODROID_BUILD
 
         TARGET_PRODUCT=$1 \
@@ -772,13 +768,13 @@ function lunch()
         # if we can't find a product, try to grab it off the LMODroid GitLab
         T=$(gettop)
         cd $T > /dev/null
-        vendor/lmodroid/build/tools/roomservice.py $product
+        $CUSTOMER_VENDOR_DIR/build/tools/roomservice.py $product
         cd - > /dev/null
         check_product $product
     else
         T=$(gettop)
         cd $T > /dev/null
-        vendor/lmodroid/build/tools/roomservice.py $product true
+        $CUSTOMER_VENDOR_DIR/build/tools/roomservice.py $product true
         cd - > /dev/null
     fi
 

AmogOS-Rom/android_build

envsetup.sh
@@ -53,8 +53,8 @@ function build_build_var_cache()
 {
     local T=$(gettop)
     # Grep out the variable names from the script.
-    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/lmodroid/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/_get_build_var_cached/) print $(i+1)}' | sort -u | tr '\n' ' '`)
-    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/lmodroid/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/_get_abs_build_var_cached/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/lmodroid/build/envsetup.sh $T/$CUSTOMER_VENDOR_DIR/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/_get_build_var_cached/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/lmodroid/build/envsetup.sh $T/$CUSTOMER_VENDOR_DIR/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/_get_abs_build_var_cached/) print $(i+1)}' | sort -u | tr '\n' ' '`)
     # Call the build system to dump the "<val>=<value>" pairs as a shell script.
     build_dicts_script=`\builtin cd $T; build/soong/soong_ui.bash --dumpvars-mode \
                         --vars="${cached_vars[*]}" \
@@ -500,13 +500,13 @@ function lunch()
         # if we can't find a product, try to grab it off the LMODroid GitLab
         T=$(gettop)
         cd $T > /dev/null
-        vendor/lmodroid/build/tools/roomservice.py $product
+        $CUSTOMER_VENDOR_DIR/build/tools/roomservice.py $product
         cd - > /dev/null
         check_product $product $release
     else
         T=$(gettop)
         cd $T > /dev/null
-        vendor/lmodroid/build/tools/roomservice.py $product true
+        $CUSTOMER_VENDOR_DIR/build/tools/roomservice.py $product true
         cd - > /dev/null
     fi
 